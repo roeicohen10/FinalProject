5a13c8732f25b45391f722fd54d7e62f06997379
roeicoh1 <roeicohen1234@gmail.com>
2021-01-07 14:13:07 +0200

    Analyze Roei 14:12 07/01/21

--- Backend/Analyze/Analyze.py
+++ Backend/Analyze/Analyze.py
@@ -5,73 +5,77 @@ from Backend.Streaming.Streaming import *
 from mpl_toolkits.mplot3d import Axes3D
 
 class Analyze:
-    def __init__(self, stats):
+    def __init__(self, stats,dataset_name,learning_algorithm_name):
         self.stats= stats
+        self.dataset_name = dataset_name
+        self.learning_algorithm_name = learning_algorithm_name
+        self.stats_list = []
+        self.stats_list.append(self.stats)
 
 
-    def show_fs_time_measures_plot(self,dataset_name,learning_algorithm_name):
+    def show_fs_time_measures_plot(self):
         batch_len = len(self.stats['fs_time_measures'])
         df = pd.DataFrame([batch_len,self.stats['fs_time_measures']])
         df = df.transpose()
         sns.scatterplot(data=df, x=df[0], y=df[1])
-        plt.title("Time taken for Feature Selection for each batch\n" + "Dataset name: " + dataset_name + "\n" + "Learning Algorithm name: " + learning_algorithm_name)
+        plt.title("Time taken for Feature Selection for each batch\n" + "Dataset name: " + self.dataset_name + "\n" + "Learning Algorithm name: " + self.learning_algorithm_name)
         plt.ylabel("Feature Selection Time")
         plt.xlabel("Batch")
         plt.show()
 
-    def show_accuracy_measures_plot(self,dataset_name,learning_algorithm_name):
+    def show_accuracy_measures_plot(self):
         batch_len = len(self.stats['acc_measures'])
         df = pd.DataFrame([batch_len,self.stats['acc_measures']])
         df = df.transpose()
         sns.scatterplot(data=df, x=df[0], y=df[1])
-        plt.title("Accuracy gained for each batch\n" + "Dataset name: " + dataset_name + "\n" + "Learning Algorithm name: " + learning_algorithm_name)
+        plt.title("Accuracy gained for each batch\n" + "Dataset name: " + self.dataset_name + "\n" + "Learning Algorithm name: " + self.learning_algorithm_name)
         plt.ylabel("Accuracy")
         plt.xlabel("Batch")
         plt.show()
 
-    def show_memory_measures_plot_for_batch(self,dataset_name,learning_algorithm_name):
+    def show_memory_measures_plot_for_batch(self):
         batch_len = len(self.stats['memory_measures'])
         df = pd.DataFrame([batch_len,self.stats['memory_measures']])
         df = df.transpose()
         sns.scatterplot(data=df, x=df[0], y=df[1])
-        plt.title("Memory used for each batch\n" + "Dataset name: " + dataset_name + "\n" + "Learning Algorithm name: " + learning_algorithm_name)
+        plt.title("Memory used for each batch\n" + "Dataset name: " + self.dataset_name + "\n" + "Learning Algorithm name: " + self.learning_algorithm_name)
         plt.ylabel("Memory Used")
         plt.xlabel("Batch")
         plt.show()
 
 
-    def show_number_of_features_for_batch(self,dataset_name,learning_algorithm_name):
+    def show_number_of_features_for_batch(self):
         num_of_features_list = [len(x) for x in self.stats["features"]]
         batch_len = len(num_of_features_list)
         df = pd.DataFrame([batch_len,num_of_features_list])
         df = df.transpose()
         sns.scatterplot(data=df,x=df[0],y=df[1])
-        plt.title("Number of features for each batch\n"+"Dataset name: "+dataset_name+"\n"+"Learning Algorithm name: "+learning_algorithm_name)
+        plt.title("Number of features for each batch\n"+"Dataset name: "+self.dataset_name+"\n"+"Learning Algorithm name: "+self.learning_algorithm_name)
         plt.ylabel("Number of Features")
         plt.xlabel("Batch")
         plt.show()
 
-    def show_accuracy_for_number_of_features(self,dataset_name,learning_algorithm_name):
+    def show_accuracy_for_number_of_features(self):
         num_of_features_list = [len(x) for x in self.stats["features"]]
         df = pd.DataFrame([num_of_features_list,self.stats['acc_measures']])
         df=df.transpose()
         sns.scatterplot(data=df,x=df[0],y=df[1])
-        plt.title("Accuracy and Number of features\n"+"Dataset name: "+dataset_name+"\n"+"Learning Algorithm name: "+learning_algorithm_name)
+        plt.title("Accuracy and Number of features\n"+"Dataset name: "+self.dataset_name+"\n"+"Learning Algorithm name: "+self.learning_algorithm_name)
         plt.xlabel("Number of Features")
         plt.ylabel("Accuracy")
         plt.show()
 
-    def show_fs_time_measures_plot(self,dataset_name,learning_algorithm_name):
+    def show_fs_time_measures_plot(self):
         batch_len = len(self.stats['proc_time_measures'])
         df = pd.DataFrame([batch_len,self.stats['proc_time_measures']])
         df = df.transpose()
         sns.scatterplot(data=df, x=df[0], y=df[1])
-        plt.title("Time taken for the process for each batch\n" + "Dataset name: " + dataset_name + "\n" + "Learning Algorithm name: " + learning_algorithm_name)
+        plt.title("Time taken for the process for each batch\n" + "Dataset name: " + self.dataset_name + "\n" + "Learning Algorithm name: " + self.learning_algorithm_name)
         plt.ylabel("Process Time")
         plt.xlabel("Batch")
         plt.show()
 
-    def show_3d_proc_time_accuracy_batch(self,dataset_name,learning_algorithm_name):
+    def show_3d_proc_time_accuracy_batch(self):
         batch_len = len(self.stats['acc_measures'])
         df = pd.DataFrame([batch_len,self.stats['acc_measures'],self.stats['proc_time_measures']])
         df = df.transpose()
@@ -81,12 +85,22 @@ class Analyze:
         x=df[0]
         y=df[1]
         z=df[2]
-        plt.title("Accuracy gained given the process time and batch number\n" + "Dataset name: " + dataset_name + "\n" + "Learning Algorithm name: " + learning_algorithm_name)
+        plt.title("Accuracy gained given the process time and batch number\n" + "Dataset name: " + self.dataset_name + "\n" + "Learning Algorithm name: " + self.learning_algorithm_name)
         ax.set_xlabel("Batch")
         ax.set_ylabel("Accuracy")
         ax.set_zlabel("Process Time")
         ax.scatter(x,y,z)
         plt.show()
+
+    def set_stats(self,stats):
+        self.stats=stats
+        self.stats_list.append(self.stats)
+
+    def set_dataset_name(self,dataset_name):
+        self.dataset_name=dataset_name
+
+    def set_learning_algorithm_name(self,learning_algorithm_name):
+        self.learning_algorithm_name=learning_algorithm_name
 
 
 
