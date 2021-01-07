119647ee051040886311b383c318a0baafeba2b7
galatedgi <gal.atedgi@gmail.com>
2021-01-07 16:23:57 +0200

    frontend

---
+++ Frontend/frontend.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from Frontend.mainWindow import Window
+root=tk.Tk()
+root.title("Final Project")
+root.geometry("1000x750")
+root.resizable(0, 0)
+window=Window(root)
+
+
+
+root.mainloop()
