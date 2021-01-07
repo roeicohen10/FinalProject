d0676a7b4ba509126965ceece9e13fdb7f47162b
roeicoh1 <roeicohen1234@gmail.com>
2021-01-07 13:47:48 +0200

    Analyze Roei 13:47 07/01/21

--- Analyze/Analyze.py
+++ Analyze/Analyze.py
@@ -1,5 +1,6 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
+import pandas as pd
 from Streaming.Streaming import *
 
 class Analyze:
@@ -15,20 +16,32 @@ class Analyze:
     def set_end_time(self,end_time):
         self.end_time=end_time
 
-    def show_time_measures_plot(self):
-        plt.plot(self.stats['time_measures'])
+    def show_time_measures_plot(self,dataset_name,learning_algorithm_name):
+        batch_len = len(self.stats['time_measures'])
+        df = pd.DataFrame([batch_len,self.stats['time_measures']])
+        df = df.transpose()
+        sns.scatterplot(data=df, x=df[0], y=df[1])
+        plt.title("Time taken for each batch\n" + "Dataset name: " + dataset_name + "\n" + "Learning Algorithm name: " + learning_algorithm_name)
         plt.ylabel("Time")
         plt.xlabel("Batch")
         plt.show()
 
-    def show_accuracy_measures_plot(self):
-        plt.plot(self.stats['acc_measures'])
+    def show_accuracy_measures_plot(self,dataset_name,learning_algorithm_name):
+        batch_len = len(self.stats['acc_measures'])
+        df = pd.DataFrame([batch_len,self.stats['acc_measures']])
+        df = df.transpose()
+        sns.scatterplot(data=df, x=df[0], y=df[1])
+        plt.title("Accuracy gained for each batch\n" + "Dataset name: " + dataset_name + "\n" + "Learning Algorithm name: " + learning_algorithm_name)
         plt.ylabel("Accuracy")
         plt.xlabel("Batch")
         plt.show()
 
-    def show_memory_measures_plot(self):
-        plt.plot(self.stats['memory_measures'])
+    def show_memory_measures_plot_for_batch(self,dataset_name,learning_algorithm_name):
+        batch_len = len(self.stats['memory_measures'])
+        df = pd.DataFrame([batch_len,self.stats['memory_measures']])
+        df = df.transpose()
+        sns.scatterplot(data=df, x=df[0], y=df[1])
+        plt.title("Memory used for each batch\n" + "Dataset name: " + dataset_name + "\n" + "Learning Algorithm name: " + learning_algorithm_name)
         plt.ylabel("Memory Used")
         plt.xlabel("Batch")
         plt.show()
@@ -36,19 +49,28 @@ class Analyze:
     def get_run_time(self):
         return self.end_time-self.start_time
 
-    def show_number_of_features(self):
+    def show_number_of_features_for_batch(self,dataset_name,learning_algorithm_name):
         num_of_features_list = [len(x) for x in self.stats["features"]]
-        plt.plot(num_of_features_list)
+        batch_len = len(num_of_features_list)
+        df = pd.DataFrame([batch_len,num_of_features_list])
+        df = df.transpose()
+        sns.scatterplot(data=df,x=df[0],y=df[1])
+        plt.title("Number of features for each batch\n"+"Dataset name: "+dataset_name+"\n"+"Learning Algorithm name: "+learning_algorithm_name)
         plt.ylabel("Number of Features")
         plt.xlabel("Batch")
         plt.show()
 
-    def show_accuracy_for_number_of_features(self):
+    def show_accuracy_for_number_of_features(self,dataset_name,learning_algorithm_name):
         num_of_features_list = [len(x) for x in self.stats["features"]]
-        plt.plot(num_of_features_list,self.stats['acc_measures'])
-        plt.ylabel("Accuracy")
+        df = pd.DataFrame([num_of_features_list,self.stats['acc_measures']])
+        df=df.transpose()
+        sns.scatterplot(data=df,x=df[0],y=df[1])
+        plt.title("Accuracy and Number of features\n"+"Dataset name: "+dataset_name+"\n"+"Learning Algorithm name: "+learning_algorithm_name)
         plt.xlabel("Number of Features")
+        plt.ylabel("Accuracy")
         plt.show()
+
+
 
 
 
